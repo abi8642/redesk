8918e3aaf93dd0a3b28dad0adb58ee5fd295256a
Abinash <abinashsahu1805@gmail.com>
2023-07-22 18:40:09 +0530

    done

--- services/pushNotification.js
+++ services/pushNotification.js
@@ -1,26 +1,61 @@
-const webPush = require("web-push");
-require("dotenv").config();
+// const webPush = require("web-push");
+// require("dotenv").config();
+
+// const publicVapidKey = process.env.PUBLIC_KEY;
+// const privateVapidKey = process.env.PRIVATE_KEY;
+
+// webPush.setVapidDetails(
+//   "mailto:abinash.sahu@apptimates.com",
+//   publicVapidKey,
+//   privateVapidKey
+// );
+
+// exports.subscribe = async (req, res) => {
+//   const subscription = req.body;
+
+//   res.status(200).send("Subscribed");
+
+//   const payload = JSON.stringify({
+//     title: "Hello World",
+//     body: "This is your first push notification",
+//   });
+
+//   webPush
+//     .sendNotification(subscription, payload)
+//     .catch((err) => console.error(err));
+// };
+
+const webpush = require("web-push");
 
 const publicVapidKey = process.env.PUBLIC_KEY;
 const privateVapidKey = process.env.PRIVATE_KEY;
 
-webPush.setVapidDetails(
-  "mailto:abinash.sahu@apptimates.com",
+webpush.setVapidDetails(
+  "mailto:abinash.sahu@apptimates.com", // Your contact email address
   publicVapidKey,
   privateVapidKey
 );
 
+// Function to send push notifications
+async function sendPushNotification(subscription, payload) {
+  try {
+    await webpush.sendNotification(subscription, JSON.stringify(payload));
+    console.log("Push notification sent successfully.");
+  } catch (error) {
+    console.error("Error sending push notification:", error);
+  }
+}
 exports.subscribe = async (req, res) => {
   const subscription = req.body;
-
-  res.status(200).send("Subscribed");
 
   const payload = JSON.stringify({
     title: "Hello World",
     body: "This is your first push notification",
   });
-
-  webPush
-    .sendNotification(subscription, payload)
-    .catch((err) => console.error(err));
+  try {
+    await webpush.sendNotification(subscription, JSON.stringify(payload));
+    console.log("Push notification sent successfully.");
+  } catch (error) {
+    console.error("Error sending push notification:", error);
+  }
 };
